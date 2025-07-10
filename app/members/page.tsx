217cc4407b944cf2317e7a706e74b13c2b1d8eb6
karel254 <kajwangbrian420@gmail.com>
2025-07-10 22:33:20 +0300

    deployment fix

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useMemo } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
